2b15feaf3b3b00bcd4a8a9cf0860da476ccb2375
davidebaldi <d.baldi88@gmail.com>
2024-11-08 09:32:54 +0100

    super cool with pug

---
+++ components/PugExperiment.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+    div
+      h1 this is pug
+      h2 works?
+      p This is a paragraph written in Pug.
+
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1 class="text-5xl text-green-800">ABOUT PAGE?</h1>
+    </div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
